d509cfe9f4a5a610586abecb30b35f29959f3a9e
Janderson de Almeida <jandersoncoelho@gmail.com>
2025-07-01 20:28:26 -0300

    feat: adiciona funcionalidade de formulário com vinculação de dados para entrada de investimentos

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrls: ['./user-input.component.css'],
 })
 export class UserInputComponent {
 
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '15';
+  duration = '10';
+
+  onReset() {
+    this.initialInvestment = '0';
+    this.duration = '10';
+    this.expectedReturn = '15';
+    this.annualInvestment = '10';
+  }
+
+  onSubmit() {
+    console.log('SUBMITED');
+    console.log('Initial Investment:', this.initialInvestment);
+    console.log('Annual Investment:', this.annualInvestment);
+    console.log('Expected Return:', this.expectedReturn);
+    console.log('Duration:', this.duration);
+  }
 }
